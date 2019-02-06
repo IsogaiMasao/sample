85e0109e597f5f78b0dc798c3f00b4534a6eb977
masao <kyara_meruru@yahoo.co.jp>
2019-02-07 03:20:54 +0900

    Laravel11の課題

---
+++ create.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>MyNews</title>
+    </head>
+    <body>
+        <h1>Myニュース作成画面</h1>
+    </body>
+</html>
+
+
+
+<!-- 課題
+1 Viewは何をするところでしょうか。簡潔に説明してみてください。
+  □Controllerの指示によって アクセスしてきたユーザーのブラウザに表示する
+  データを生成する ところ
+
+2 プログラマーがhtmlを書かずにPHPなどのプログラミング言語や
+　　　　フレームワークを使う必要があるのはどういった理由でしょうか。
+　 □ユーザー毎の情報を表示したり、様々な機能を実装したい場合にはHTMLだけでは出来ない。
+　  また、フレームワークを使うことによってPHPを書く場合に１００行必要なところ、
+　  １０行ですんだりする。＊コードの書き方を統一させるため
+
+　　 Codeigniter（コードイグニッター）
+　　 CakePHP（ケイクピーエイチピー）
+　　 Symfony（シンフォニー）
+　　 ZendFramework（ゼンドフレームワーク）
+　　 FuelPHP（フューエルピーエイチピー）
+
+
+3 【応用】 前々章でAdmin/ProfileControllerの
+　　　　　　add Action, edit Action に次のように記述しました。(中略)
+          add Action と edit Action を描画するには、
+          それぞれどこのディレクトリに何というbladeファイルを設置すれば良いでしょうか。
+  □　mkdir -p ./resources/views/admin/profile
+  □  mkdir -p ./resources/views/admin/edit
+
+
+
+4 【応用】 3. の答えを実際に作成してみましょう。
+   また、作成したbladeファイルにhtmlで記述して装飾してみましょう。
+
+
+
+-->
